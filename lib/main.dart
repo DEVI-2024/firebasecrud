2e881714256bdce66acd599e16fd63b5a021522c
Devi-2024 <devidevika9982@gmail.com>
2024-07-28 18:07:35 +0530

    remove space

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        //home: const TodoList(),
         home: const HomeView(),
         debugShowCheckedModeBanner: false,
       ),
